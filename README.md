a0536d0d5177e1c80550cad921e3eb91eaa1d93c
yuriy.deomidov <yuriy.deomidov@exquance.com>
2018-05-10 18:28:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ShinyStackBarTest
---
+++ ShinyStackBarTest/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShinyStackBarTest
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            PopulateData();
+
+        }
+
+        private void PopulateData()
+        {
+            _floors = new ObservableCollection<Floor>();
+            for (int i = 4; i > 0; i--)
+            {
+                Floor floor = new Floor($"floor{i}");
+                for (int j = 1; j < 8 - i; j++)
+                {
+                    Room room = new Room($"room{i}{j}", (i * 95.5 * j % 7) * 10.0, (RoomType)((i + j) % 4));
+                    floor.Rooms.Add(room);
+                }
+                _floors.Add(floor);
+            }
+        }
+
+        private ObservableCollection<Floor> _floors;
+        public ObservableCollection<Floor> Floors { get { return _floors; } }
+    }
+}
